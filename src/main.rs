986652532ecae53d7197448ba1849f27dcc4ea5a
jaewak <jaewan.0907@gmail.com>
2025-01-19 04:49:07 -0500

    create a basic call

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use yahoo_finance_api::YahooConnector;
+
+#[tokio::main]
+async fn main() {
+	let yahoo = match YahooConnector::new() {
+		Ok(connector) => connector,
+		Err(e) => {
+			println!("Failed to initialize YahooConnector: {}", e);
+			return;
+		}
+	};
+
+	match yahoo.get_latest_quotes("AAPL", "1d").await {
+		Ok(response) => {
+			println!("Stock info ${}", response.last_quote().unwrap().close);
+		}
+		Err(e) => {
+			println!("Error: {}", e);
+		}
+	}
 }
